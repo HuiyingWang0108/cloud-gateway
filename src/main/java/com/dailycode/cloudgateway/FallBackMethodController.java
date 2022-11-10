f027d85a2304d4fcf68f96af40dbd19b15bc556f
Huiying Wang <whymissu1228@gmail.com>
2022-11-10 10:23:30 -0800

    DI config-server & updated configuration

--- src/main/java/com/dailycode/cloudgateway/FallBackMethodController.java
+++ src/main/java/com/dailycode/cloudgateway/FallBackMethodController.java
@@ -3,10 +3,18 @@ package com.dailycode.cloudgateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 @RestController
 public class FallBackMethodController {
 
-    @GetMapping("/userServiceFallBack")
+    // @GetMapping("/userServiceFallBack")
+    // @HystrixCommand(fallbackMethod = "userServiceFallBackMethod_fallback",
+    //     commandProperties = {
+    //         @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", 
+    //         value = "4000")
+    // })
     public String userServiceFallBackMethod() {
 
         return "User Service is taking longer than expected."+
